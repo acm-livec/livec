f214fd6afe29c95561ccbd8f8ead1f99a2a6dbb5
jramir7254 <83803191+jramir7254@users.noreply.github.com>
2025-07-27 23:32:20 -0600

    Pulled Box Updates

--- frontend/src/components/SuggestionBox.jsx
+++ frontend/src/components/SuggestionBox.jsx
@@ -21,13 +21,9 @@ export default function SuggestionBox({ sectionId }) {
     return (
         <div className={styles['suggestion-box']}>
             {!user && <Placeholder />}
-            {response && response.success === true && (
-                <Success onClose={close} response={response} />
-            )}
+            {response && response.success === true && <Success onClose={close} response={response} />}
             {response && response.success === false && <Failure />}
-            {user && !response && (
-                <Default sectionId={sectionId} submit={submit} />
-            )}
+            {user && !response && <Default sectionId={sectionId} submit={submit} />}
         </div>
     );
 }
@@ -51,38 +47,51 @@ const Default = ({ sectionId, submit }) => {
             label: 'Update Existing Knowledge Area',
             value: 'update-existing-knowledge-area',
         },
+        {
+            label: 'Add Suggested Learning Outcomes',
+            value: 'add-learning-outcomes',
+        },
+        {
+            label: 'Revise Learning Outcomes',
+            value: 'revise-learning-outcomes',
+        },
+        {
+            label: 'Suggest Industry Alignment',
+            value: 'suggest-industry-alignment',
+        },
+        {
+            label: 'Include Interdisciplinary Topics',
+            value: 'include-interdisciplinary-topics',
+        },
+        {
+            label: 'Propose Teaching Methodologies',
+            value: 'propose-teaching-methodologies',
+        },
+        {
+            label: 'Suggest Emerging Technologies or Trends',
+            value: 'suggest-emerging-trends',
+        },
+        {
+            label: 'Propose Assessment Techniques',
+            value: 'propose-assessment-techniques',
+        },
+        {
+            label: 'Request Clarification or Guidance',
+            value: 'request-clarification',
+        },
+        {
+            label: 'General Feedback or Comment',
+            value: 'general-feedback',
+        },
     ];
 
     return (
         <>
-            <h2>
-                Have a thought or suggestion for this section? Fill out the form
-                below.
-            </h2>
-            <Form
-                resetOn={[sectionId]}
-                onSubmit={(formData) =>
-                    submit({ sectionId, discipline, ...formData })
-                }
-            >
-                <Dropdown
-                    keyName="type"
-                    values={options}
-                    label="Type of suggestion"
-                    required
-                />
-                <TextField
-                    keyName="title"
-                    label="Enter a brief title explaining your suggestions"
-                    required
-                    maxLength={100}
-                />
-                <TextArea
-                    keyName="text"
-                    label="Enter your suggestion down below"
-                    required
-                    maxLength={500}
-                />
+            <h2>Have a thought or suggestion for this section? Fill out the form below.</h2>
+            <Form resetOn={[sectionId]} onSubmit={(formData) => submit({ sectionId, discipline, ...formData })}>
+                <Dropdown keyName="type" values={options} label="Type of suggestion" required />
+                <TextField keyName="title" label="Enter a brief title explaining your suggestions" required maxLength={100} />
+                <TextArea keyName="text" label="Enter your suggestion down below" required maxLength={500} />
             </Form>
         </>
     );
